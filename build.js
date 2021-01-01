2c16a9249e895098c8094996de07a7dc46168dfc
r3ticuli <messs@seas.upenn.edu>
2021-01-01 16:13:27 -0500

    semantic templating

--- build.js
+++ build.js
@@ -11,6 +11,30 @@ const homeTemplate = Handlebars.compile(homeTemplateFile);
 const headerTemplate = fs.readFileSync("./src/templates/header.hbs").toString();
 Handlebars.registerPartial("header", headerTemplate);
 
+const workTemplate = fs.readFileSync("./src/templates/work.hbs").toString();
+Handlebars.registerPartial("work", workTemplate);
+
+const educationTemplate = fs.readFileSync("./src/templates/education.hbs").toString();
+Handlebars.registerPartial("education", educationTemplate);
+
+const skillsTemplate = fs.readFileSync("./src/templates/skills.hbs").toString();
+Handlebars.registerPartial("skills", skillsTemplate);
+
+const languagesTemplate = fs.readFileSync("./src/templates/languages.hbs").toString();
+Handlebars.registerPartial("languages", languagesTemplate);
+
+const interestsTemplate = fs.readFileSync("./src/templates/interests.hbs").toString();
+Handlebars.registerPartial("interests", interestsTemplate);
+
+// Register Helpers
+
+Handlebars.registerHelper("link", function (url) {
+  var url = Handlebars.escapeExpression(url);
+  var text = url.split("//")[1];
+
+  return new Handlebars.SafeString("<a href='" + url + "'>" + text + "</a>");
+});
+
 // load json-resume
 
 const context = fs.readFileSync("./resume.json").toString();
