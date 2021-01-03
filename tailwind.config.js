9e7d924f6f954d1353d9c5d5bfecfa43dcf9d7b7
r3ticuli <messs@seas.upenn.edu>
2021-01-03 15:29:56 -0500

    added tailwind + sass

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.hbs", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
